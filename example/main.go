b4643a6283624c550616450ca07908558ebd7c38
Coby Benveniste <coby@base.la>
2022-05-16 15:14:05 +0300

    Add size of nano q

--- example/main.go
+++ example/main.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"unsafe"
 
+	"github.com/probably-not/q/nano"
 	"github.com/probably-not/q/pico"
 )
 
 func main() {
 	picoQ := pico.NewQ()
+	nanoQ := nano.NewQ()
 	fmt.Println("=========================== Queue Memory Sizes ===========================")
 	fmt.Println("PicoQ:", unsafe.Sizeof(picoQ), "bytes")
+	fmt.Println("NanoQ:", unsafe.Sizeof(nanoQ), "bytes")
 	fmt.Println("==========================================================================")
 }
